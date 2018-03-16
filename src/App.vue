c93cc14f2b7411390fa8e7f2d0f660dc3eb03892
Mick Jasker <m.jasker@student.fontys.nl>
2018-03-16 14:37:24 +0100

    AnimeJS test

--- src/App.vue
+++ src/App.vue
@@ -6,44 +6,11 @@
 
 <script>
   import anime from 'animejs'
-
-
   export default {
-    name: 'App',
-    methods: {
-      animate () {
-        anime({
-          targets: '#move',
-          rotate: 250,
-          loop: true,
-          autoplay: true
-        })
-      }
-
-    },
-    created () {
-      anime({
-        targets: '#move',
-        translateX: [
-          { value: 100, duration: 1200 },
-          { value: 0, duration: 800 }
-        ],
-        rotate: '1turn',
-        backgroundColor: '#FFF',
-        duration: 2000,
-        loop: true
-      })
-    }
+    name: 'App'
   }
 </script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-</style>
+
+  </style>
